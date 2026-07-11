3c7c8b3699c47914027ee2af95707ecc0e685334
Logren12 <Logren12@gmail.com>
2026-07-11 17:28:56 +0200

    Initialize project.

---
+++ src/main/java/pl/logren12/broadside/BroadsideApplication.java
@@ -0,0 +1,13 @@
+package pl.logren12.broadside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroadsideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BroadsideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/logren12/broadside/BroadsideApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.logren12.broadside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroadsideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
