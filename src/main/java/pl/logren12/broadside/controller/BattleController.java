8cdf0e11fd1025e7c15cc321698a15d62d87d04e
Logren12 <Logren12@gmail.com>
2026-09-22 17:09:27 +0200

    Set logical endpoints to controllers, add basic validation for inputs.

--- src/main/java/pl/logren12/broadside/controller/BattleController.java
+++ src/main/java/pl/logren12/broadside/controller/BattleController.java
@@ -8,7 +8,7 @@ import pl.logren12.broadside.repository.BattleRepository;
 import pl.logren12.broadside.service.BattleService;
 
 @RestController
-@RequestMapping("/battles")
+@RequestMapping("/api/battles")
 public class BattleController {
     // services
     private final BattleService battleService;
@@ -24,7 +24,7 @@ public class BattleController {
         return this.battleRepository.findById(battleId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Battle not found"));
     }
-    @PostMapping("/create-battle")
+    @PostMapping
     public Battle createBattle(@RequestParam String captain1Name, @RequestParam String captain2Name){
         // todo Check whether captains exist in database
         return battleService.startABattle(captain1Name, captain2Name);
--- src/main/java/pl/logren12/broadside/controller/CaptainController.java
+++ src/main/java/pl/logren12/broadside/controller/CaptainController.java
@@ -1,6 +1,9 @@
 package pl.logren12.broadside.controller;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import pl.logren12.broadside.model.*;
 import pl.logren12.broadside.repository.CaptainRepository;
@@ -8,23 +11,23 @@ import pl.logren12.broadside.service.AiService;
 import pl.logren12.broadside.service.PlayerService;
 
 import java.util.List;
-
+@Validated
 @RestController
-@RequestMapping("/campaign")
-public class CampaignController {
+@RequestMapping("/api/captains")
+public class CaptainController {
 
     private final PlayerService playerService;
     private final AiService aiService;
     private final CaptainRepository captainRepository;
 
-    public CampaignController(PlayerService playerService, AiService aiService, CaptainRepository captainRepository) {
+    public CaptainController(PlayerService playerService, AiService aiService, CaptainRepository captainRepository) {
         this.playerService = playerService;
         this.aiService = aiService;
         this.captainRepository = captainRepository;
     }
 
-    @PostMapping("/create-captain")
-    public Captain createCaptain(@RequestParam String name,
+    @PostMapping
+    public Captain createCaptain(@RequestParam @NotBlank(message = "Name must not be blank") @Size(max=25) String name,
                                  @RequestParam Faction faction,
                                  @RequestParam ShipType shipType,
                                  @RequestParam(defaultValue = "true") boolean isBot) {
@@ -35,19 +38,22 @@ public class CampaignController {
             return this.playerService.createPlayerCaptain(name, faction, shipType,0,0);
         }
     }
-    @GetMapping("/captains")
-    public ResponseEntity<List<Captain>> statCheck(
-            @RequestParam(required = false) Long id,
+    @GetMapping
+    public ResponseEntity<List<Captain>> searchCaptains(
             @RequestParam(required = false) String captainName,
             @RequestParam(required = false) Faction faction) {
         //todo Implement search logic for multiple arguments
-        if (id != null) {
-            return captainRepository.findById(id).map(List::of).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-        } else if (captainName != null && !captainName.isBlank()) {
+        if (captainName != null && !captainName.isBlank()) {
             return ResponseEntity.ok(captainRepository.findByName(captainName));
         } else if (faction != null) {
             return ResponseEntity.ok(captainRepository.findByFaction(faction));
         }
         return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/{captainId}")
+    public ResponseEntity<Captain> getCaptain(@PathVariable Long captainId) {
+        return captainRepository.findById(captainId).
+                map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
