9374371b646458590b988d8df28d8df898df97a3
Logren12 <Logren12@gmail.com>
2026-09-24 01:29:08 +0200

    Move talking with repositories to services and unify controller - service - repo architecture

--- src/main/java/pl/logren12/broadside/controller/CaptainController.java
+++ src/main/java/pl/logren12/broadside/controller/CaptainController.java
@@ -2,13 +2,10 @@ package pl.logren12.broadside.controller;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import pl.logren12.broadside.model.*;
-import pl.logren12.broadside.repository.CaptainRepository;
-import pl.logren12.broadside.service.AiService;
-import pl.logren12.broadside.service.PlayerService;
+import pl.logren12.broadside.service.CaptainService;
 
 import java.util.List;
 @Validated
@@ -16,44 +13,36 @@ import java.util.List;
 @RequestMapping("/api/captains")
 public class CaptainController {
 
-    private final PlayerService playerService;
-    private final AiService aiService;
-    private final CaptainRepository captainRepository;
+    private final CaptainService captainService;
 
-    public CaptainController(PlayerService playerService, AiService aiService, CaptainRepository captainRepository) {
-        this.playerService = playerService;
-        this.aiService = aiService;
-        this.captainRepository = captainRepository;
+    public CaptainController(CaptainService captainService) {
+        this.captainService = captainService;
     }
 
     @PostMapping
     public Captain createCaptain(@RequestParam @NotBlank(message = "Name must not be blank") @Size(max=25) String name,
                                  @RequestParam Faction faction,
                                  @RequestParam ShipType shipType,
-                                 @RequestParam(defaultValue = "true") boolean isBot) {
-        if (isBot){
-            return this.aiService.createAiCaptain(name, faction, shipType);
+                                 @RequestParam(defaultValue = "true") boolean bot) {
+        //todo Add/implement modifiers
+        if (bot){
+            return this.captainService.create(name, faction, shipType, 0,0,true);
         }
         else{
-            return this.playerService.createPlayerCaptain(name, faction, shipType,0,0);
+            return this.captainService.create(name, faction, shipType,0,0, false);
         }
     }
     @GetMapping
-    public ResponseEntity<List<Captain>> searchCaptains(
-            @RequestParam(required = false) String captainName,
-            @RequestParam(required = false) Faction faction) {
-        //todo Implement search logic for multiple arguments
-        if (captainName != null && !captainName.isBlank()) {
-            return ResponseEntity.ok(captainRepository.findByName(captainName));
-        } else if (faction != null) {
-            return ResponseEntity.ok(captainRepository.findByFaction(faction));
-        }
-        return ResponseEntity.badRequest().build();
+    public List<Captain> searchCaptains(
+            @RequestParam(required = false) @Size(max = 25) String name,
+            @RequestParam(required = false) Faction faction,
+            @RequestParam(required = false) Boolean bot) //Boolean and not boolean to have 3 states to make it possible to search for non bots! Diffrentiate omitting filter and speficying false
+    {
+        return captainService.search(name, faction, bot);
     }
 
     @GetMapping("/{captainId}")
-    public ResponseEntity<Captain> getCaptain(@PathVariable Long captainId) {
-        return captainRepository.findById(captainId).
-                map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    public Captain getCaptain(@PathVariable long captainId) {
+        return captainService.getById(captainId);
     }
 }
