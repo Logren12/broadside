0cd81c331b4cec9a142c2f97bdaaf909b2009147
logren <logren12@gmail.com>
2026-09-23 16:25:59 +0200

    Add leveling up for captains

--- src/main/java/pl/logren12/broadside/service/PlayerService.java
+++ src/main/java/pl/logren12/broadside/service/PlayerService.java
@@ -1,10 +1,8 @@
 package pl.logren12.broadside.service;
 
 import org.springframework.stereotype.Service;
-import pl.logren12.broadside.model.Captain;
-import pl.logren12.broadside.model.Faction;
-import pl.logren12.broadside.model.Ship;
-import pl.logren12.broadside.model.ShipType;
+import org.springframework.util.Assert;
+import pl.logren12.broadside.model.*;
 import pl.logren12.broadside.repository.CaptainRepository;
 
 @Service
@@ -14,8 +12,16 @@ public class PlayerService {
     public PlayerService(CaptainRepository captainRepository) {
         this.captainRepository = captainRepository;
     }
+
     public Captain createPlayerCaptain(String name, Faction faction, ShipType shipType, int sailingModifier, int leadershipModifier){
         Captain newCaptain = new Captain(name, faction, new Ship(shipType), sailingModifier, leadershipModifier, false);
         return this.captainRepository.save(newCaptain);
+    }
+
+    public void levelUpCaptain(Captain captain, SkillType skillType){
+        // toDo Czy tutaj nie powinienem przypadkiem otworzyć transakcji?
+        Assert.notNull(captain, "Captain cannot be null");
+        Assert.notNull(skillType, "Skill type cannot be null");
+        captain.levelUp(skillType);
     }
 }
